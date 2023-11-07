5989e8dd50d392c203a91db89b5fa7f555229e6b
Ningen09 <85623808+Ningen09@users.noreply.github.com>
2023-11-07 11:45:38 +0900

    commit for merge

--- src/mocks/chef-search-list.mock.ts
+++ src/mocks/chef-search-list.mock.ts
@@ -1,5 +1,5 @@
 import { ChefItem } from "Types";
-import chefListMock from "./Chef-list.mock";
+import chefListMock from "./chef-list.mock";
 
 const chefSearchListMock = (word:string): ChefItem[] => {
     const list = chefListMock.filter(boardItem => boardItem.chefNickname.includes(word));
